d4e8b94302355ebabfe65f2bd1411b2793da7c28
liorBaru <62611078+liorBaru@users.noreply.github.com>
2020-04-07 21:37:00 +0300

    Update DepthFirstSearch.java

--- src/algorithms/search/DepthFirstSearch.java
+++ src/algorithms/search/DepthFirstSearch.java
@@ -1,5 +1,44 @@
 package algorithms.search;
 
-public class DepthFirstSearch {
+import java.util.HashMap;
+import java.util.Stack;
 
+public class DepthFirstSearch extends ASearchingAlgorithm{
+
+    protected Stack<AState> stack;
+
+    @Override
+    public Solution solve(ISearchable domain) {
+        if (domain == null) return null;
+
+        stack = new Stack<>();
+        states = new HashMap<>();
+        AState goalState = DFS(domain);
+        //System.out.println("before back tracking");
+        return goalState != null ? backTrackingToStartState(goalState) : null ;
+    }
+
+    private AState DFS(ISearchable domain){
+        stack.push(domain.getStartState());
+        states.put(domain.getStartState().toString() , domain.getStartState());
+
+        while (!stack.isEmpty()){
+
+            AState currentState = stack.pop();
+
+            if (currentState.equals(domain.getGoalState()))
+                return currentState;
+
+            if (!states.containsKey(currentState.toString()))
+                states.put(currentState.toString() , currentState);
+
+            for (AState s : domain.getAllPossibleStates(currentState)) {
+                if (!states.containsKey(s.toString())) {
+                    //System.out.println(s.toString());
+                    stack.push(s);
+                }
+            }
+        }
+        return null;
+    }
 }
