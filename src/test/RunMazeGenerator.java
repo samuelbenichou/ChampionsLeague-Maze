da5da77794496f0f3e389aa2e3ea6e41d380db94
samuelbenichou <samuelbenichou@hotmail.fr>
2020-04-12 17:24:54 +0300

    MazeGenerators finish. Need to check SimpleMazeGenerator

--- src/test/RunMazeGenerator.java
+++ src/test/RunMazeGenerator.java
@@ -7,6 +7,7 @@ import algorithms.mazeGenerators.*;
  */
 public class RunMazeGenerator {
     public static void main(String[] args) {
+        testMazeGenerator(new EmptyMazeGenerator());
         testMazeGenerator(new SimpleMazeGenerator());
         testMazeGenerator(new MyMazeGenerator());
         /*EmptyMazeGenerator a = new EmptyMazeGenerator();
@@ -29,10 +30,10 @@ public class RunMazeGenerator {
         // prints the time it takes the algorithm to run
         System.out.println(String.format("Maze generation time(ms): %s", mazeGenerator.measureAlgorithmTimeMillis(100/*rows*/,100/*columns*/)));
         // generate another maze
-        Maze maze = mazeGenerator.generate(100/*rows*/, 100/*columns*/);
+        Maze maze = mazeGenerator.generate(1000/*rows*/, 1000/*columns*/);
 
         // prints the maze
-        maze.print();
+        //maze.print();
 
         // get the maze entrance
         Position startPosition = maze.getStartPosition();
